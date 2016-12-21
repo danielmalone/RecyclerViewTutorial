90d1c90869c78254df65a8303178230b07907982
Daniel Malone <danieljmalone@gmail.com>
2016-12-21 13:36:52 -0600

    take snapshot for 2

--- app/src/main/java/com/finepointmobile/recyclerviewtutorial/MainActivity.java
+++ app/src/main/java/com/finepointmobile/recyclerviewtutorial/MainActivity.java
@@ -2,8 +2,13 @@ package com.finepointmobile.recyclerviewtutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
