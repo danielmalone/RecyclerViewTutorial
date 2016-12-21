57dc300a16692fe57adedd36702dc6303af8d797
Daniel Malone <danieljmalone@gmail.com>
2016-12-21 13:48:41 -0600

    after linking recyclerview to java

--- app/src/main/java/com/finepointmobile/recyclerviewtutorial/MainActivity.java
+++ app/src/main/java/com/finepointmobile/recyclerviewtutorial/MainActivity.java
@@ -1,8 +1,11 @@
 package com.finepointmobile.recyclerviewtutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(data);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
